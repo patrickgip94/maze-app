8571cd650f2ec6062c1b66faaf9df85ca1096afb
Patrick Gip <gippatrick@gmail.com>
2022-10-30 12:26:49 -0700

    added thirdweb utilities

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import network from "../utils/network";
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ import Header from "../components/Header";
 const Home = () => {
   return (
     <div className="">
+      {/* Header */}
       <Header />
+
+      
     </div>
   )
 };
