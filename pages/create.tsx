82e5baa246d32c6ff6ac4552d34fd650acc6a1de
Patrick Gip <gippatrick@gmail.com>
2022-10-31 21:00:45 -0700

    added direct listing and auction function

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import { 
   useAddress, 
@@ -10,6 +10,7 @@ import {
   useCreateAuctionListing,
   useCreateDirectListing,
 } from "@thirdweb-dev/react";
+import { NFT } from '@thirdweb-dev/sdk';
 
 
 type Props = {}
@@ -20,14 +21,72 @@ function Create ({}: Props) {
     process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT,
     'marketplace'
   );
+  const [selectedNft, setSelectedNft] = useState<NFT>();
+
+  const { contract: collectionContract } = useContract(
+    process.env.NEXT_PUBLIC_COLLECTION,
+    'nft-collection'
+  );
+
+  const ownedNfts = useOwnedNFTs(collectionContract, address);
 
   return (
     <div>
       <Header />
 
-      <main>
-        <h1>List an Item</h1>
-        <h2>Select an Item you would like to Sell</h2>
+      <main className="max-w-6xl mx-auto p-10 pt-2">
+        <h1 className="text-4xl font-bold">List an Item</h1>
+        <h2 className="text-xl font-semibold pt-5">Select an Item you would like to Sell</h2>
+
+        <hr className="mb-5" />
+
+        <p>Below you will find the NFT's you own in your wallet</p>
+        <div className="flex overflow-x-scroll space-x-2 p-4">
+          {ownedNfts?.data?.map(nft => (
+            <div 
+              key={nft.metadata.id}
+              onClick={() => setSelectedNft(nft)}
+              className={`flex flex-col space-y-2 card min-w-fit border-2 bg-gray-100 ${nft.
+                metadata.id === selectedNft?.metadata.id 
+                ? "border-black" 
+                : "border-transparent"
+              }`}
+            >
+              <MediaRenderer 
+                className="h-48 rounded-lg" 
+                src={nft.metadata.image} 
+              />
+              <p className="text-lg truncate font-bold">{nft.metadata.name}</p>
+              <p className="text-xs truncate">{nft.metadata.description}</p>
+            </div>
+          ))}
+        </div>
+
+        {selectedNft && (
+          <form>
+            <div>
+              <div className="flex flex-col p-10">
+                <label>Direct Listing / Fixed Price</label>
+                <input 
+                  type="radio" 
+                  name="listingType" 
+                  value="directListing"
+                  className="ml-auto h-10 w-10" 
+                />
+
+                <label>Auction</label>
+                <input 
+                  type="radio" 
+                  name="listingType" 
+                  value="auctionListing"
+                  className="ml-auto h-10 w-10" 
+                />
+
+
+              </div>
+            </div>
+          </form>
+        )}
       </main>
     </div>
   )
