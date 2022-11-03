b9b130a9d550845c43188c3b115412184eedfb1f
Patrick Gip <gippatrick@gmail.com>
2022-11-02 17:15:24 -0700

    created offer button

--- pages/listing/[listingId].tsx
+++ pages/listing/[listingId].tsx
@@ -10,8 +10,9 @@ import {
   MediaRenderer,
   useAddress, 
   useListing,
+  useAcceptDirectListingOffer
 } from '@thirdweb-dev/react';
-import { ListingType } from '@thirdweb-dev/sdk';
+import { ListingType, NATIVE_TOKENS } from '@thirdweb-dev/sdk';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
@@ -21,6 +22,8 @@ import { ethers } from 'ethers';
 
 function ListingPage() {
   const router = useRouter();
+  const address = useAddress();
+
   const { listingId } = router.query as { listingId: string };
   const [bidAmount, setBidAmount] = useState('');
   const [, switchNetwork] = useNetwork();
@@ -37,14 +40,17 @@ function ListingPage() {
     'marketplace'
     );
 
-    const { mutate: makeBid } = useMakeBid(contract);
+  const { mutate: makeBid } = useMakeBid(contract);
 
-  const { data: offers } = useOffers(contract, listingId);
-  console.log(offers);
+  const  {data: offers} = useOffers(contract, listingId);
 
   const { mutate: makeOffer } = useMakeOffer(contract);
+
   const { mutate: buyNow } = useBuyNow(contract);
+
   const { data: listing, isLoading, error } = useListing(contract, listingId);
+
+  const { mutate: acceptOffer } = useAcceptDirectListingOffer(contract);
 
   useEffect (() => {
     if (!listingId || !contract || !listing) return;
@@ -228,7 +234,52 @@ function ListingPage() {
           </div>
 
           {/* TODO: If DIRECT, show offers here... */}
-          
+          {listing.type === ListingType.Direct && offers && (
+            <div className="grid grid-cols-2 gap-y-2">
+              <p className="font-bold">Offers: </p>
+              <p className="font-bold">
+                {offers.length > 0 ? offers.length : 0}
+              </p>
+
+              {offers.map((offer) => (
+                <>
+                  <p className="flex items-center ml-5 text-sm italic">
+                    <UserCircleIcon className="h-3 mr-2" />
+                    {offer.offeror.slice(0, 5) + 
+                      "..." +
+                      offer.offeror.slice(-5)}
+                  </p>
+                  <div>
+                    <p
+                      key={
+                        offer.listingId +
+                        offer.offeror +
+                        offer.totalOfferAmount.toString()
+                      }
+                      className="text-sm italic"
+                    >
+                      {ethers.utils.formatEther(offer.totalOfferAmount)}{" "}
+                      {NATIVE_TOKENS[network].symbol}
+                    </p>
+                    {listing.sellerAddress === address && (
+                      <button 
+                        onClick={() => {
+                          acceptOffer({
+                            listingId,
+                            addressOfOfferor: offer.offeror,
+                          });
+                        }}
+                        className="p-2 w-32 bg-red-500/50 rounded-lg font-bold text-xs cursor-pointer"
+                      >
+                        Accept Offer
+                      </button>
+                    )}
+                  </div>
+                </>
+              ))}
+            </div>
+          )}
+
           <div className="grid grid-cols-2 space-y-2 items-center justify-end">
 
             <hr className='col-span-2'/>
@@ -240,6 +291,7 @@ function ListingPage() {
             </p>
 
             {/* TODO: Remaining time on auction goes here */}
+
             {listing.type === ListingType.Auction && (
               <>
                 <p>Current Minimum Bid: </p>
